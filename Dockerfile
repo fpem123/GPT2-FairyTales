8d9c40b846be303caa3424d9650e9b2545e92849
Hanjeong Lee <gkswjdzz@naver.com>
2021-03-16 16:20:45 +0900

    remove using tokenizer, transformers

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
-FROM leehoseop/gpt2_fairytale:1.0
+# FROM leehoseop/gpt2_fairytale:1.0
+FROM python:3.7
 
 WORKDIR /app
+RUN pip install flask
+
 COPY . .
 
 EXPOSE 80
--- main.py
+++ main.py
@@ -5,134 +5,134 @@
     update: 21.02.14
 '''
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
+# from transformers import AutoModelForCausalLM, AutoTokenizer
 from flask import Flask, request, jsonify, render_template
-import torch
-import os
-from queue import Queue, Empty
-from threading import Thread
-import time
+# import torch
+# import os
+# from queue import Queue, Empty
+# from threading import Thread
+# import time
 
 app = Flask(__name__)
 
-print("model loading...")
+# print("model loading...")
 
-print(os.system("ls"))
+# print(os.system("ls"))
 
-# Model & Tokenizer loading
-tokenizer = AutoTokenizer.from_pretrained('./GPT2-large_Fairytale')
-model = AutoModelForCausalLM.from_pretrained('./GPT2-large_Fairytale')
+## Model & Tokenizer loading
+# tokenizer = AutoTokenizer.from_pretrained('./GPT2-large_Fairytale')
+# model = AutoModelForCausalLM.from_pretrained('./GPT2-large_Fairytale')
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model.to(device)
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# model.to(device)
 
-requests_queue = Queue()    # request queue.
-BATCH_SIZE = 1              # max request size.
-CHECK_INTERVAL = 0.1
+# requests_queue = Queue()    # request queue.
+# BATCH_SIZE = 1              # max request size.
+# CHECK_INTERVAL = 0.1
 
-print("complete model loading")
-
-
-##
-# Request handler.
-# GPU app can process only one request in one time.
-def handle_requests_by_batch():
-    while True:
-        request_batch = []
-
-        while not (len(request_batch) >= BATCH_SIZE):
-            try:
-                request_batch.append(requests_queue.get(timeout=CHECK_INTERVAL))
-            except Empty:
-                continue
-
-            for requests in request_batch:
-                try:
-                    requests["output"] = mk_fairytale(requests['input'][0], requests['input'][1])
-
-                except Exception as e:
-                    requests["output"] = e
+# print("complete model loading")
 
 
-handler = Thread(target=handle_requests_by_batch).start()
+# ##
+# # Request handler.
+# # GPU app can process only one request in one time.
+# def handle_requests_by_batch():
+#     while True:
+#         request_batch = []
+
+#         while not (len(request_batch) >= BATCH_SIZE):
+#             try:
+#                 request_batch.append(requests_queue.get(timeout=CHECK_INTERVAL))
+#             except Empty:
+#                 continue
+
+#             for requests in request_batch:
+#                 try:
+#                     requests["output"] = mk_fairytale(requests['input'][0], requests['input'][1])
+
+#                 except Exception as e:
+#                     requests["output"] = e
 
 
-##
-# GPT-2 generator.
-# Make Fairytale
-def mk_fairytale(text, length):
-    try:
-        input_ids = tokenizer.encode(text, return_tensors='pt')
-
-        # input_ids also need to apply gpu device!
-        input_ids = input_ids.to(device)
-
-        min_length = len(input_ids.tolist()[0])
-
-        length = length if length > 0 else 1
-
-        length += min_length
-
-        # story model generating
-        outputs = model.generate(input_ids, pad_token_id=50256,
-                                 do_sample=True,
-                                 max_length=length,
-                                 min_length=min_length,
-                                 top_k=40,
-                                 num_return_sequences=1)
-
-        result = dict()
-
-        for idx, sample_output in enumerate(outputs):
-            result[0] = tokenizer.decode(sample_output.tolist(), skip_special_tokens=True)
-
-        return result
-
-    except Exception as e:
-        print('Error occur in script generating!', e)
-        return jsonify({'error': e}), 500
+# handler = Thread(target=handle_requests_by_batch).start()
 
 
-##
-# Get post request page.
-@app.route('/fairytale', methods=['POST'])
-def generate():
+# ##
+# # GPT-2 generator.
+# # Make Fairytale
+# def mk_fairytale(text, length):
+#     try:
+#         input_ids = tokenizer.encode(text, return_tensors='pt')
 
-    # GPU app can process only one request in one time.
-    if requests_queue.qsize() > BATCH_SIZE:
-        return jsonify({'Error': 'Too Many Requests'}), 429
+#         # input_ids also need to apply gpu device!
+#         input_ids = input_ids.to(device)
 
-    try:
-        args = []
+#         min_length = len(input_ids.tolist()[0])
 
-        text = request.form['text']
-        length = int(request.form['length'])
+#         length = length if length > 0 else 1
 
-        args.append(text)
-        args.append(length)
+#         length += min_length
 
-    except Exception as e:
-        return jsonify({'message': 'Invalid request'}), 500
+#         # story model generating
+#         outputs = model.generate(input_ids, pad_token_id=50256,
+#                                  do_sample=True,
+#                                  max_length=length,
+#                                  min_length=min_length,
+#                                  top_k=40,
+#                                  num_return_sequences=1)
 
-    # input a request on queue
-    req = {'input': args}
-    requests_queue.put(req)
+#         result = dict()
 
-    # wait
-    while 'output' not in req:
-        time.sleep(CHECK_INTERVAL)
+#         for idx, sample_output in enumerate(outputs):
+#             result[0] = tokenizer.decode(sample_output.tolist(), skip_special_tokens=True)
 
-    return jsonify(req['output'])
+#         return result
+
+#     except Exception as e:
+#         print('Error occur in script generating!', e)
+#         return jsonify({'error': e}), 500
 
 
-##
-# Queue deadlock error debug page.
-@app.route('/queue_clear')
-def queue_clear():
-    while not requests_queue.empty():
-        requests_queue.get()
+# ##
+# # Get post request page.
+# @app.route('/fairytale', methods=['POST'])
+# def generate():
 
-    return "Clear", 200
+#     # GPU app can process only one request in one time.
+#     if requests_queue.qsize() > BATCH_SIZE:
+#         return jsonify({'Error': 'Too Many Requests'}), 429
+
+#     try:
+#         args = []
+
+#         text = request.form['text']
+#         length = int(request.form['length'])
+
+#         args.append(text)
+#         args.append(length)
+
+#     except Exception as e:
+#         return jsonify({'message': 'Invalid request'}), 500
+
+#     # input a request on queue
+#     req = {'input': args}
+#     requests_queue.put(req)
+
+#     # wait
+#     while 'output' not in req:
+#         time.sleep(CHECK_INTERVAL)
+
+#     return jsonify(req['output'])
+
+
+# ##
+# # Queue deadlock error debug page.
+# @app.route('/queue_clear')
+# def queue_clear():
+#     while not requests_queue.empty():
+#         requests_queue.get()
+
+#     return "Clear", 200
 
 
 ##
@@ -150,5 +150,4 @@ def main():
 
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, port=80, host='0.0.0.0')
+    app.run(port=80, host='0.0.0.0')
